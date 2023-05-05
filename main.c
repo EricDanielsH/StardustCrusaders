335f8718d856d52662891f8285f562d1a2f4824c
Daniels Huaman <danieled@vdi-scclab-041.lancs.local>
2023-05-05 10:23:49 +0100

    Start of the main file, where all written will be coded

---
+++ main.c
@@ -0,0 +1,12 @@
+//create structs
+//struct of the spaceship
+//struct of the blackhole
+
+//player 1 = player on the left down side of the screen = ^ v < >
+//player 2 = player on the right up side of the screen = v ^ < >
+
+/* to diferentiate the two rockets, they will have different colours
+* if im very advanced, let the player choose their colors
+* when an arrow key is pressed, add or subtract from the position and aslo change the character to the designated orientation
+*/
+
